62d3397b3a6042f0c67c70c8b30ce5da68f285e9
Son Daehyeon <sondaehyeon01@gmail.com>
2025-04-25 21:28:31 +0900

    fix(#11): 메인 화면에서 최근 행사 레이아웃 밀림

--- src/app/_component/all-events.tsx
+++ src/app/_component/all-events.tsx
@@ -1,32 +1,32 @@
-import { useEffect, useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 
 import EventFilter from '@/app/_component/event-filter';
 
-import Api from '@/api';
 import { EventDto } from '@/api/dto/event';
 import { Category } from '@/api/schema/event';
 import { District } from '@/api/schema/user';
 
-import { useApi } from '@/hook/use-api';
-
 import EventCard from '@/component/event-card';
 
-export default function AllEvents() {
-  const [, startApi] = useApi();
+interface AllEventsProps {
+  categories: Category[] | undefined;
+  districts: District[] | undefined;
+  isFree: undefined | boolean;
+  setCategories: Dispatch<SetStateAction<Category[] | undefined>>;
+  setDistricts: Dispatch<SetStateAction<District[] | undefined>>;
+  setIsFree: Dispatch<SetStateAction<boolean | undefined>>;
+  events: EventDto[];
+}
 
-  const [categories, setCategories] = useState<Category[]>();
-  const [districts, setDistricts] = useState<District[]>();
-  const [isFree, setIsFree] = useState<boolean>();
-
-  const [events, setEvents] = useState<EventDto[]>([]);
-
-  useEffect(() => {
-    startApi(async () => {
-      const { events } = await Api.Domain.Event.getEvents(undefined, categories, districts, isFree);
-      setEvents(events);
-    });
-  }, [categories, districts, isFree]);
-
+export default function AllEvents({
+  categories,
+  districts,
+  isFree,
+  setCategories,
+  setDistricts,
+  setIsFree,
+  events,
+}: AllEventsProps) {
   return (
     <section className="flex max-w-screen-xl flex-col justify-center gap-2 sm:gap-4">
       <h2 className="text-2xl font-bold sm:text-3xl">전체 행사</h2>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,60 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import AllEvents from '@/app/_component/all-events';
 import Hero from '@/app/_component/hero';
 import HotEvent from '@/app/_component/hot-event';
 
+import Api from '@/api';
+import { EventDto } from '@/api/dto/event';
+import { Category } from '@/api/schema/event';
+import { District } from '@/api/schema/user';
+
+import { useApi } from '@/hook/use-api';
+
 export default function Page() {
+  const [isApiProcessing, startApi] = useApi();
+  const [, startApi2] = useApi();
+
+  const [hotEvents, setHotEvents] = useState<EventDto[]>([]);
+  const [allEvents, setAllEvents] = useState<EventDto[]>([]);
+
+  const [categories, setCategories] = useState<Category[]>();
+  const [districts, setDistricts] = useState<District[]>();
+  const [isFree, setIsFree] = useState<boolean>();
+
+  useEffect(() => {
+    startApi(async () => {
+      const { events } = await Api.Domain.Event.getEvents();
+      setHotEvents(events);
+    });
+  }, []);
+
+  useEffect(() => {
+    startApi2(async () => {
+      const { events } = await Api.Domain.Event.getEvents(undefined, categories, districts, isFree);
+      setAllEvents(events);
+    });
+  }, [categories, districts, isFree]);
+
   return (
     <div className="flex flex-col items-center gap-8 sm:gap-16">
       <Hero />
-      <HotEvent />
-      <AllEvents />
+      {!isApiProcessing && (
+        <>
+          <HotEvent events={hotEvents} />
+          <AllEvents
+            categories={categories}
+            districts={districts}
+            isFree={isFree}
+            setCategories={setCategories}
+            setDistricts={setDistricts}
+            setIsFree={setIsFree}
+            events={allEvents}
+          />
+        </>
+      )}
     </div>
   );
 }
