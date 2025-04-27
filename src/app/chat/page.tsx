46b3eca2b3ae70b17141da00fce8f56a36cb4b6d
Son Daehyeon <sondaehyeon01@gmail.com>
2025-04-27 18:25:31 +0900

    fix(guard): 채팅 페이지에 로그인되지 않은 유저가 접근되던 이슈

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -16,6 +16,8 @@ import { useRoomStore } from '@/store/chat.store';
 import { useApi } from '@/hook/use-api';
 import useMobile from '@/hook/use-mobile';
 
+import UserGuard from '@/lib/guard/user.guard';
+
 import { motion } from 'framer-motion';
 import { MessageSquare } from 'lucide-react';
 
@@ -36,8 +38,9 @@ export default function Page() {
       const { chats } = await Api.Domain.Chat.getChatInfo(meetingId);
       setChats(chats);
 
-      await Api.Domain.Chat.readAllChat(meetingId);
-      readAllRoom(meetingId);
+      Api.Domain.Chat.readAllChat(meetingId).then(() => {
+        readAllRoom(meetingId);
+      });
     });
   }, [meetingId]);
 
@@ -51,42 +54,44 @@ export default function Page() {
   }, [meetingId, chats]);
 
   return (
-    <motion.div
-      className="container mx-auto grid h-[calc(100dvh-56px-16px)] max-w-screen-xl grid-cols-1 gap-4 sm:h-[calc(100dvh-64px-32px)] sm:grid-cols-3"
-      initial={{ y: -20, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-    >
-      {(!isMobile || !meetingId) && (
-        <div className="flex flex-col overflow-clip rounded-2xl border sm:col-span-1">
-          {rooms.map((room) => (
-            <RoomCard key={room.meeting.id} room={room} meetingId={meetingId} />
-          ))}
-        </div>
-      )}
+    <UserGuard>
+      <motion.div
+        className="container mx-auto grid h-[calc(100dvh-56px-16px)] max-w-screen-xl grid-cols-1 gap-4 sm:h-[calc(100dvh-64px-32px)] sm:grid-cols-3"
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        {(!isMobile || !meetingId) && (
+          <div className="flex flex-col overflow-clip rounded-2xl border sm:col-span-1">
+            {rooms.map((room) => (
+              <RoomCard key={room.meeting.id} room={room} meetingId={meetingId} />
+            ))}
+          </div>
+        )}
 
-      {(!isMobile || meetingId) && (
-        <div className="flex flex-col overflow-hidden rounded-xl border sm:col-span-2">
-          {isApiProcessing ? (
-            <div className="flex h-full items-center justify-center">
-              <Spinner />
-            </div>
-          ) : meetingId && chats ? (
-            <Chatting
-              room={rooms.find((room) => room.meeting.id === meetingId)!}
-              chats={chats}
-              setChats={setChats}
-            />
-          ) : (
-            <div className="flex h-full flex-col items-center justify-center gap-4">
-              <MessageSquare className="size-10" />
-              <div className="flex flex-col items-center">
-                <h3 className="text-xl font-medium">채팅방을 선택해주세요</h3>
-                <p className="text-neutral-500">왼쪽 목록에서 채팅방을 선택해주세요</p>
+        {(!isMobile || meetingId) && (
+          <div className="flex flex-col overflow-hidden rounded-xl border sm:col-span-2">
+            {isApiProcessing ? (
+              <div className="flex h-full items-center justify-center">
+                <Spinner />
               </div>
-            </div>
-          )}
-        </div>
-      )}
-    </motion.div>
+            ) : meetingId && chats ? (
+              <Chatting
+                room={rooms.find((room) => room.meeting.id === meetingId)!}
+                chats={chats}
+                setChats={setChats}
+              />
+            ) : (
+              <div className="flex h-full flex-col items-center justify-center gap-4">
+                <MessageSquare className="size-10" />
+                <div className="flex flex-col items-center">
+                  <h3 className="text-xl font-medium">채팅방을 선택해주세요</h3>
+                  <p className="text-neutral-500">왼쪽 목록에서 채팅방을 선택해주세요</p>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </motion.div>
+    </UserGuard>
   );
 }
